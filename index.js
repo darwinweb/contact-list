6c3622208e69b074d6492f78ef55a349f0b29487
Darwin Gutierrez <darwindrum@gmail.com>
2023-08-21 17:56:25 -0500

    se modifico la funcion agregar contactos

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-let listaContactos = [
-    "Darwin Gutierrez",
-    "Yamile Tovar",
-    "Emmanuel Gutierrez",
-    "Abigail Gutierrez",
-    
-];
+let listaContactos = [];
 
-function añadirContacto(nombreCompleto){
-    listaContactos.push(nombreCompleto)
+function añadirContacto(id, nombre, apellido, telefono, ciudad, direccion){
+    let nuevoContacto = {
+        id: id,
+        nombre: nombre,
+        apellidos: apellido,
+        telefono: telefono,
+        ubicaciones:[
+            {
+                ciudad: ciudad,
+                direccion: direccion
+            }
+        ]
+
+    };
+    listaContactos.push(nuevoContacto);
 }
-añadirContacto("Alejandro Castro");
-
-
-function borrarContacto(quitar){
-    listaContactos.splice(quitar, 1);
-}
-borrarContacto(4);
+añadirContacto(1,"Darwin","Gutierrez", 3012753041, "Cartagena","Conjunto Ipanema");
+añadirContacto(2, "valentina", "gomez", 3024353625, "medellin","El poblado");
 
 
 for (let i = 0; i < listaContactos.length; i++){
